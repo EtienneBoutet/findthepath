e650d84860e5f1ccd98f7e89aeb690eb9efc9fb7
Etienne Boutet <etienne.boutet@outlook.com>
2018-11-28 17:59:53 -0500

    the program is finding the path but not always the best one, i'll find out why later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import math
 import pygame
+from pprint import pprint
 
 # Constants
 WINDOW_SIZE = [510, 510]
@@ -18,6 +20,11 @@ class Node:
     def __init__(self):
         self._color = None
         self._coordinates = None
+        self._grid_coordinates = None
+        self._distance_to_end = None
+        self._cost = 0
+        self._total_weight = 0
+        self._parent_node = None
 
     @property
     def color(self):
@@ -35,11 +42,52 @@ class Node:
     def coordinates(self, value):
         self._coordinates = value
 
+    @property
+    def distance_to_end(self):
+        return self._distance_to_end
+
+    @distance_to_end.setter
+    def distance_to_end(self, value):
+        self._distance_to_end = value
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @cost.setter
+    def cost(self, value):
+        self._cost = value
+
+    @property
+    def grid_coordinates(self):
+        return self._grid_coordinates
+
+    @grid_coordinates.setter
+    def grid_coordinates(self, value):
+        self._grid_coordinates = value
+
+    @property
+    def total_weight(self):
+        return self._total_weight
+
+    @total_weight.setter
+    def total_weight(self, value):
+        self._total_weight = value
+
+    @property
+    def parent_node(self):
+        return self._parent_node
+
+    @parent_node.setter
+    def parent_node(self, value):
+        self._parent_node = value
+
+
 '''
     Find the [x, y] coordinates of a node
 '''
 def get_node_coordinates(row, column):
-    return [row * WIDTH, column * HEIGHT]
+    return [row * (WIDTH + MARGIN), column * (HEIGHT + MARGIN)]
 
 '''
     Will handle every click made on the grid and return a modified grid array.
@@ -51,7 +99,6 @@ def handle_click_on_grid(grid, click_color):
         pos = pygame.mouse.get_pos()
         column = pos[0] // (WIDTH + MARGIN)
         row = pos[1] // (HEIGHT + MARGIN)
-
         if not grid[row][column].color:
             grid[row][column].color = click_color
             if click_color == 1:
@@ -75,12 +122,47 @@ def draw_grid(grid, screen):
                 color = RED
             elif node.color == 3:
                 color = BLACK
+            elif node.color == 4:
+                color = (255, 10, 10)
             pygame.draw.rect(screen,
                              color,
                              [(MARGIN + WIDTH) * column + MARGIN,
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
                               HEIGHT])
+
+'''
+    Return a list of the adjacent nodes of a node.
+'''
+def find_adjacent_squares(grid, original_node, open_list, closed_list):
+    node_row = original_node.grid_coordinates[0]
+    node_column = original_node.grid_coordinates[1]
+
+    for i in range(-1, 2):
+
+        for j in range(-1, 2):
+            adjacent_node_row = node_row + i
+            adjacent_node_column = node_column + j
+
+            if 0 <= adjacent_node_row <= 19 and 0 <= adjacent_node_column <= 19:
+                adjacent_node = grid[adjacent_node_row][adjacent_node_column]
+
+                if adjacent_node not in closed_list and adjacent_node.color != 3:
+                    if adjacent_node in open_list:
+                        new_total_weight = (original_node.cost + 1) + adjacent_node.distance_to_end
+
+                        if new_total_weight < adjacent_node.total_weight:
+                            adjacent_node.cost = original_node.cost + 1
+                            adjacent_node.total_weight = adjacent_node.cost + adjacent_node.distance_to_end
+                            adjacent_node.parent_node = original_node
+
+                    else:
+                        adjacent_node.cost = original_node.cost + 1
+                        adjacent_node.total_weight = adjacent_node.cost + adjacent_node.distance_to_end
+                        adjacent_node.parent_node = original_node
+                        adjacent_node.color = 4
+                        open_list.append(adjacent_node)
+    return open_list
 '''
     Find the best path to get to the end cell and will modify the grid's cell if they are in the best path
 '''
@@ -88,24 +170,50 @@ def path_finding(grid):
     nodes = []
     for row in grid:
         for node in row:
+            node.grid_coordinates = [grid.index(row), row.index(node)]
             node.coordinates = get_node_coordinates(grid.index(row), row.index(node))
             nodes.append(node)
-
-    # Find the start node
-    start_node = None
-    for node in nodes:
-        if node.color == 2:
-            start_node = node
-            break
 
     # Find the end node
     end_node = None
     for node in nodes:
-        if node.color == 1:
+        if node.color == 2:
             end_node = node
             break
 
+    # Find the end node
+    start_node = None
+    for node in nodes:
+        if node.color == 1:
+            start_node = node
+            break
 
+    for node in nodes:
+        x_distance = end_node.coordinates[0] - node.coordinates[0]
+        y_distance = end_node.coordinates[1] - node.coordinates[1]
+
+        node.distance_to_end = math.sqrt((x_distance ** 2 )+ (y_distance ** 2))
+
+    # Find the start node
+
+    closed_list = [start_node]
+    open_list = []
+
+    open_list = find_adjacent_squares(grid, start_node, open_list, closed_list)
+
+    while open_list:
+        if end_node in open_list:
+            parent = end_node.parent_node
+            while parent is not None:
+                parent.color = 1
+                parent = parent.parent_node
+            break
+
+        open_list.sort(key = lambda x: x.total_weight, reverse=False)
+        current_node = open_list[0]
+        open_list.remove(current_node)
+        closed_list.append(current_node)
+        open_list = find_adjacent_squares(grid, current_node, open_list, closed_list)
 
 '''
     Find the position of a cell in the grid.
